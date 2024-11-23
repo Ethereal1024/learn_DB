85da6359eb3a4a1e7a45ee99ea826ad8e14187dc
Ethereal <2893734607@qq.com>
2024-11-23 07:49:35 +0000

    completed lru_replacer.cpp

--- UniBase_dev/src/replacer/lru_replacer.cpp
+++ UniBase_dev/src/replacer/lru_replacer.cpp
@@ -2,7 +2,7 @@
 
 LRUReplacer::LRUReplacer(size_t num_pages) { max_size_ = num_pages; }
 
-LRUReplacer::~LRUReplacer() = default;  
+LRUReplacer::~LRUReplacer() = default;
 
 /**
  * @description: 使用LRU策略删除一个victim frame，并返回该frame的id
@@ -12,12 +12,15 @@ LRUReplacer::~LRUReplacer() = default;
 bool LRUReplacer::victim(frame_id_t* frame_id) {
     // C++17 std::scoped_lock
     // 它能够避免死锁发生，其构造函数能够自动进行上锁操作，析构函数会对互斥量进行解锁操作，保证线程安全。
-    std::scoped_lock lock{latch_};  //  如果编译报错可以替换成其他lock
+    std::scoped_lock lock{latch_};
 
-    // Todo:
-    //  利用lru_replacer中的LRUlist_,LRUHash_实现LRU策略
-    //  选择合适的frame指定为淘汰页面,赋值给*frame_id
+    if (LRUlist_.empty()) {
+        return false;
+    }
 
+    *frame_id = LRUlist_.back();
+    LRUlist_.pop_back();
+    LRUhash_.erase(*frame_id);
     return true;
 }
 
@@ -27,9 +30,10 @@ bool LRUReplacer::victim(frame_id_t* frame_id) {
  */
 void LRUReplacer::pin(frame_id_t frame_id) {
     std::scoped_lock lock{latch_};
-    // Todo:
-    // 固定指定id的frame
-    // 在数据结构中移除该frame
+    if (LRUhash_.find(frame_id) != LRUhash_.end()) {
+        LRUlist_.erase(LRUhash_[frame_id]);
+        LRUhash_.erase(frame_id);
+    }
 }
 
 /**
@@ -37,12 +41,17 @@ void LRUReplacer::pin(frame_id_t frame_id) {
  * @param {frame_id_t} frame_id 取消固定的frame的id
  */
 void LRUReplacer::unpin(frame_id_t frame_id) {
-    // Todo:
-    //  支持并发锁
-    //  选择一个frame取消固定
+    std::scoped_lock lock{latch_};
+    if (LRUhash_.find(frame_id) == LRUhash_.end()) {
+        LRUlist_.push_front(frame_id);
+        LRUhash_[frame_id] = LRUlist_.begin();
+    }
 }
 
 /**
  * @description: 获取当前replacer中可以被淘汰的页面数量
  */
-size_t LRUReplacer::Size() { return LRUlist_.size(); }
+size_t LRUReplacer::Size() {
+    std::scoped_lock lock{latch_};
+    return LRUlist_.size();
+}
